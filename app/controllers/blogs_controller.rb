f2885d088091a4fa2cce193b0f65d518c3e302d1
mapi8808 <mapi.008.k@gmail.com>
2020-11-12 08:55:21 +0000

    [add]3-6

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -22,6 +22,11 @@ end
     @blog = Blog.find(params[:id])
   end
   
+  def update
+    blog = Blog.find(params[:id])
+    blog.update(blog_params)
+    redirect_to blog_path(blog)
+  
    private
   def blog_params
     params.require(:blog).permit(:title, :category, :body)
